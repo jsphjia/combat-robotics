ef43f78aea474796be3fa693aa6f40f9104b2dd5
jsphjia <joseph.jia23@gmail.com>
2024-10-04 11:09:52 -0400

    v1 commit
    initial dataset and partial code for training yolo model

---
+++ 9-29-data/README.dataset.txt
@@ -0,0 +1,6 @@
+# Brick Detection Original View > 2024-09-30 12:58am
+https://universe.roboflow.com/test-5ev0m/brick-detection-original-view
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ house-robot.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+from roboflow import Roboflow
+import numpy as np
+import supervision as sv
+
+def process_frame(frame: np.ndarray, model):
+    results = model(frame, imgsz = 1280)[0]
+    detections = sv.Detections.from_yolov8(results)
+    box_annotator = sv.BoxAnnotator(thickness = 4, text_thickness = 4, text_scale = 2)
+
+
+    return frame
+
+def main():
+    # import most up-to-date dataset from roboflow
+    rf = Roboflow(api_key="ysXcOkuwq46DKP58MBEg")
+    project = rf.workspace("test-5ev0m").project("brick-detection-original-view")
+    version = project.version(2)
+    dataset = version.download("yolov8")
+
+    # training YOLO model
+    model = YOLO("yolov8s.pt")
+    model.train(data = "9-29-data/data.yaml", epochs = 100)
+
+    # import video to test house robot detection
+    test_vid_path = "" # add downloaded video path later
+    vid_info = sv.VideoInfo.from_video_path(test_vid_path)
+
+    sv.process_video(source_path = test_vid_path, target_path = "results.mp4", callback = process_frame)
